c7eb70e644f9814e77d21657c52cd07e4a4d0852
tsukifell <calvinsaputra217@gmail.com>
2023-04-09 19:22:43 +0700

    initial commit

---
+++ app/src/main/java/com/cepotdev/dicory/ui/activity/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.cepotdev.dicory.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cepotdev.dicory.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+}
