a52a5908bdd644fb736d2aa98c8a85ec8d587580
tsukifell <calvinsaputra217@gmail.com>
2023-04-17 00:02:48 +0700

    Failed to give the detailed data

--- app/src/main/java/com/cepotdev/dicory/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/cepotdev/dicory/viewmodel/LoginViewModel.kt
@@ -32,7 +32,7 @@ class LoginViewModel : ViewModel() {
                     _loginResponse.postValue(
                         LoginResponse(
                             error = true,
-                            message = "The email or password might be wrong"
+                            message = "The email or password might be wrong!"
                         )
                     )
                 }
